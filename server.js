24c270fae4e3832c3ecf6c92e920be76c785ebc2
rittik <rittik.dey@bouvet.no>
2014-10-25 12:01:11 +0200

    Added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app     = express();
+var bodyParser = require('body-parser');
+
+
+
+app.use(express.static(__dirname + '/sender')); 		// set the static files location /public/img will be /img for users
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//require('./app/routes')(app);
+
+app.listen('8081')
+console.log('Server connected at 8081');
